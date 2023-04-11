7595048fed5f6707f667097ac2091bd4d7fade12
Umasankar <39300990+umasankar-ts@users.noreply.github.com>
2023-04-12 00:59:51 +0530

    Removed public folder

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.listen(port);
